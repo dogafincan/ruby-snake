70a35fbbe5b700edffefb0c179769d72f0534518
Doga Fincan <doga@icloud.com>
2017-07-21 11:57:25 +0200

    Add game_over method to Field class

--- snake.rb
+++ snake.rb
@@ -4,28 +4,40 @@ class Field
   attr_reader :part, :area
 
   def initialize
-  end
-
-  Curses.init_screen
-  Curses.noecho
-
-  begin
     
-  ensure
-    Curses.close_screen
   end
+
+  def game_over
+    x = Curses.cols / 2
+    y = Curses.lines / 2
+    Curses.setpos(y, x)
+    Curses.addstr('Game Over')
+    Curses.getch
+  end
+
 end
 
 class Snake
-    attr_reader :part, :size, :direction
+  attr_reader :part, :size, :direction
 
-    def initialize
-    end
+  def initialize
+  end
 end
 
 class Apple
-    attr_reader :part, :location
+  attr_reader :part, :location
 
-    def initialize
-    end
+  def initialize
+  end
+end
+
+Curses.init_screen
+Curses.noecho
+Curses.cbreak
+
+begin
+  field = Field.new
+  field.game_over
+ensure
+  Curses.close_screen
 end
